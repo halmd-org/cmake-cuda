528a2a9f58cc310ee437258219e54273f80239ec
Peter Colberg <pcolberg@chem.utoronto.ca>
2012-05-19 17:05:03 -0400

    Support CUDA package components
    This commit adds support for finding a subset of CUDA libraries.
    
    find_package(CUDA QUIET REQUIRED COMPONENTS cuda)
    
    find_package(CUDA QUIET REQUIRED COMPONENTS cudart)
    
    find_package(CUDA QUIET REQUIRED COMPONENTS cuda cudart)
    
    For the last example, the following variables will be set:
    
    CUDA_CUDA_FOUND
    CUDA_CUDA_LIBRARY
    CUDA_CUDART_FOUND
    CUDA_CUDART_LIBRARY
    
    For backwards compatibility with earlier CMake-CUDA versions,
    it is recommended to link programs against ${CUDA_LIBRARIES}.

--- Modules/FindCUDA.cmake
+++ Modules/FindCUDA.cmake
@@ -5,13 +5,15 @@
 #  CUDA_FOUND
 #  CUDA_INCLUDE_DIR
 #  CUDA_LIBRARIES
-#  CUDA_LIBRARY
-#  CUDA_RUNTIME_LIBRARY
+#  CUDA_CUDA_FOUND
+#  CUDA_CUDA_LIBRARY
+#  CUDA_CUDART_FOUND
+#  CUDA_CUDART_LIBRARY
 #
 
 #=============================================================================
 # Copyright 2002-2009 Kitware, Inc.
-# Copyright 2008-2011 Peter Colberg
+# Copyright 2008-2012 Peter Colberg
 #
 # Distributed under the OSI-approved BSD License (the "License");
 # see accompanying file Copyright.txt for details.
@@ -23,7 +25,16 @@
 # (To distribute this file outside of CMake, substitute the full
 #  License text for the above reference.)
 
-FIND_PATH(CUDA_INCLUDE_DIR cuda_runtime.h
+if(NOT CUDA_FIND_COMPONENTS)
+  set(CUDA_FIND_COMPONENTS cuda cudart)
+  if(CUDA_FIND_REQUIRED)
+    foreach(comp ${CUDA_FIND_COMPONENTS})
+      set(CUDA_FIND_REQUIRED_${comp} TRUE)
+    endforeach()
+  endif()
+endif()
+
+find_path(CUDA_INCLUDE_DIR cuda_runtime.h
   HINTS
   $ENV{CUDA_TOOLKIT_ROOT_DIR}
   PATH_SUFFIXES include/cuda include
@@ -39,55 +50,44 @@ FIND_PATH(CUDA_INCLUDE_DIR cuda_runtime.h
   /opt
   /opt/cuda
 )
+mark_as_advanced(CUDA_INCLUDE_DIR)
 
-FIND_LIBRARY(CUDA_LIBRARY NAMES cuda
-  HINTS
-  $ENV{CUDA_TOOLKIT_ROOT_DIR}
-  PATH_SUFFIXES lib64 lib
-  PATHS
-  ~/Library/Frameworks
-  /Library/Frameworks
-  /usr/local
-  /usr/local/cuda
-  /usr
-  /sw # Fink
-  /opt/local # DarwinPorts
-  /opt/csw # Blastwave
-  /opt
-  /opt/cuda
-)
+set(CUDA_LIBRARIES)
 
-FIND_LIBRARY(CUDA_RUNTIME_LIBRARY NAMES cudart
-  HINTS
-  $ENV{CUDA_TOOLKIT_ROOT_DIR}
-  PATH_SUFFIXES lib64 lib
-  PATHS
-  ~/Library/Frameworks
-  /Library/Frameworks
-  /usr/local
-  /usr/local/cuda
-  /usr
-  /sw # Fink
-  /opt/local # DarwinPorts
-  /opt/csw # Blastwave
-  /opt
-  /opt/cuda
-)
+foreach(comp ${CUDA_FIND_COMPONENTS})
+  string(TOUPPER ${comp} var)
+  find_library(CUDA_${var}_LIBRARY NAMES ${comp}
+    HINTS
+    $ENV{CUDA_TOOLKIT_ROOT_DIR}
+    PATH_SUFFIXES lib64 lib
+    PATHS
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local
+    /usr/local/cuda
+    /usr
+    /sw # Fink
+    /opt/local # DarwinPorts
+    /opt/csw # Blastwave
+    /opt
+    /opt/cuda
+  )
+  mark_as_advanced(CUDA_${var}_LIBRARY)
+  if(CUDA_${var}_LIBRARY)
+    list(APPEND CUDA_LIBRARIES ${CUDA_${var}_LIBRARY})
+    set(CUDA_${var}_FOUND TRUE)
+    set(CUDA_${comp}_FOUND TRUE)
+  else()
+    set(CUDA_${var}_FOUND FALSE)
+    set(CUDA_${comp}_FOUND FALSE)
+  endif()
+  unset(var)
+endforeach()
 
-IF(CUDA_LIBRARY AND CUDA_RUNTIME_LIBRARY)
-  SET(CUDA_LIBRARIES ${CUDA_LIBRARY} ${CUDA_RUNTIME_LIBRARY})
-ENDIF()
+include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
 
-INCLUDE(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
+find_package_handle_standard_args(CUDA HANDLE_COMPONENTS REQUIRED_VARS CUDA_INCLUDE_DIR)
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(CUDA DEFAULT_MSG
-  CUDA_INCLUDE_DIR
-  CUDA_LIBRARY
-  CUDA_RUNTIME_LIBRARY
-)
-
-MARK_AS_ADVANCED(
-  CUDA_INCLUDE_DIR
-  CUDA_LIBRARY
-  CUDA_RUNTIME_LIBRARY
-)
+foreach(comp ${CUDA_FIND_COMPONENTS})
+  unset(CUDA_${comp}_FOUND)
+endforeach()
